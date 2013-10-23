61c33f66ca452dad30adacd078ced430f985f37d
jalex <jalex2006@gmail.com>
2013-10-23 18:44:13 +0400

    first commit

---
+++ RelyingParties/RelyingParty1/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IdentityModel.Services;
+using System.Web.Mvc;
+
+namespace RelyingParty1.Controllers {
+
+    public class AccountController: Controller {
+
+        public ActionResult Login() {
+            var authModule = FederatedAuthentication.WSFederationAuthenticationModule;
+            var signInRequestMessage = authModule.CreateSignInRequest(null, null, false);
+
+            return Redirect(signInRequestMessage.RequestUrl);
+        }
+
+        public ActionResult Logout() {
+            var authModule = FederatedAuthentication.WSFederationAuthenticationModule;
+            authModule.SignOut(false);
+            var signOutRequestMessage = new SignOutRequestMessage(new Uri(authModule.Issuer), authModule.Realm); // initiate federated sign out request to the STS
+            var redirectUrl = signOutRequestMessage.WriteQueryString();
+            return Redirect(redirectUrl);
+        }
+
+    }
+}
---
+++ RelyingParties/RelyingParty1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace RelyingParty1.Controllers {
+
+    public class HomeController: Controller {
+
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
